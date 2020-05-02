d90523bf04236c7094d4551a1f90fa675f805fb8
itsamanverma <amanvermame786@gmail.com>
2020-05-03 02:12:17 +0530

    fetch the video data from local

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import classes from './App.css';
+import video from './videos/sculpture.mp4';
 
 function App() {
 
-  const [data, setData] = useState( [] ) 
-  useEffect(() => {
-    const fetchData = async() => {
-      const result = await fetch("https://orangevalleycaa.org/api/videos").then(
-        response => response.json()
-      )
-      setData(result);
-    };
-    fetchData(); 
-  }, []);
+  // const [data, setData] = useState( [] ) 
+  // useEffect(() => {
+  //   const fetchData = async() => {
+  //     const result = await fetch("https://orangevalleycaa.org/api/videos").then(
+  //       response => response.json()
+  //     )
+  //     setData(result);
+  //   };
+  //   fetchData(); 
+  // }, []);
 
   return (
     <div className={classes.App}>
       <header>
         <h1>video</h1>
       </header>
-      {data.map(video => (
+      {/* {data.map(video => (
         <div key={video.id}>
           <h2>{video.name}</h2>
           <video height={200} controls src={video.video_url} />
         </div>
-      ))}
+      ))} */}
+      <div className={classes.Video}>
+        <video src={video} controls height={200}/>
+      </div>
     </div>
   );
 }
